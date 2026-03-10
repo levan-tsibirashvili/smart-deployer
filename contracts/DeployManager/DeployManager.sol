93f672a79c4d5a377f9ee06afa4d3e03030a13b3
levan-tsibirashvili <levan.tsibirashvili@gmail.com>
2026-03-10 16:11:48 +0400

    update contracts

---
+++ contracts/DeployManager/DeployManager.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "../UtilityContract/IUtilityContract.sol";
+import "./IDeployManager.sol";
+
+/// @title DeployManager - Factory for utility contracts
+/// @author Levan Tsibirashvili
+/// @notice Allows users to deploy utility contracts by cloning registered templates.
+/// @dev Uses OpenZeppelin's Clones and Ownable; assumes templates implement IUtilityContract.
+contract DeployManager is IDeployManager, Ownable, ERC165 {
+    constructor() payable Ownable(msg.sender) {}
+
+    /// @dev Stores registered contracts information
+    struct ContractInfo {
+        uint256 fee; /// @notice Deployment fee (in wei)
+        bool isDeployable; /// @notice Shows deployable status
+        uint256 registeredAt; /// @notice Registration timestamp
+    }
+
+    /// @dev Maps deployer address to an array of deployed contracts addresses
+    mapping(address => address[]) public deployedContracts;
+
+    /// @dev Maps registered contract address to it's registratation
+    mapping(address => ContractInfo) public contractsData;
+
+    /// @inheritdoc IDeployManager
+    function deploy(address _utilityContract, bytes calldata _initData) external payable override returns (address) {
+        ContractInfo memory info = contractsData[_utilityContract];
+
+        require(info.isDeployable, ContractNotActive());
+        require(msg.value >= info.fee, NotEnoughtFunds());
+        require(info.registeredAt > 0, ContractDoesNotRegistered());
+
+        address clone = Clones.clone(_utilityContract);
+
+        require(IUtilityContract(clone).initialize(_initData), InitializationFailed());
+
+        payable(owner()).transfer(msg.value);
+
+        deployedContracts[msg.sender].push(clone);
+
+        emit NewDeployment(msg.sender, clone, msg.value, block.timestamp);
+
+        return clone;
+    }
+
+    /// @inheritdoc IDeployManager
+    function addNewContract(address _contractAddress, uint256 _fee, bool _isActive) external override onlyOwner {
+        require(
+            IUtilityContract(_contractAddress).supportsInterface(type(IUtilityContract).interfaceId),
+            ContractIsNotUtilityContract()
+        );
+        require(contractsData[_contractAddress].registeredAt == 0, AlreadyRegistered());
+
+        contractsData[_contractAddress] = ContractInfo({fee: _fee, isDeployable: _isActive, registeredAt: block.timestamp});
+
+        emit NewContractAdded(_contractAddress, _fee, _isActive, block.timestamp);
+    }
+
+    /// @inheritdoc IDeployManager
+    function updateFee(address _contractAddress, uint256 _newFee) external override onlyOwner {
+        require(contractsData[_contractAddress].registeredAt > 0, ContractDoesNotRegistered());
+
+        uint256 _oldFee = contractsData[_contractAddress].fee;
+        contractsData[_contractAddress].fee = _newFee;
+
+        emit ContractFeeUpdated(_contractAddress, _oldFee, _newFee, block.timestamp);
+    }
+
+    /// @inheritdoc IDeployManager
+    function deactivateContract(address _address) external override onlyOwner {
+        require(contractsData[_address].registeredAt > 0, ContractDoesNotRegistered());
+
+        contractsData[_address].isDeployable = false;
+
+        emit ContractStatusUpdated(_address, false, block.timestamp);
+    }
+
+    /// @inheritdoc IDeployManager
+    function activateContract(address _address) external override onlyOwner {
+        require(contractsData[_address].registeredAt > 0, ContractDoesNotRegistered());
+
+        contractsData[_address].isDeployable = true;
+
+        emit ContractStatusUpdated(_address, true, block.timestamp);
+    }
+
+    /// @inheritdoc ERC165
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC165) returns (bool) {
+        return interfaceId == type(IDeployManager).interfaceId || super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/ERC20Airdroper/ERC20Airdroper.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import "../UtilityContract/AbstractUtilityContract.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title ERC20Airdroper - Utility contract for batch ERC20 token distribution
+/// @author Levan Tsibirashvili
+/// @notice Enables owners to airdrop ERC20 tokens to multiple recipients
+contract ERC20Airdroper is AbstractUtilityContract, Ownable {
+    /// @notice Initializes Ownable with deployer
+    constructor() payable Ownable(msg.sender) {}
+
+    /// @notice Maximum number of token transfers per airdrop call
+    uint256 public constant MAX_AIRDROP_BATCH_SIZE = 300;
+
+    /// @notice ERC20 token contract to distribute from
+    IERC20 public token;
+
+    /// @notice Fixed amount used for allowance check
+    uint256 public amount;
+
+    /// @notice Address holding tokens to be distributed
+    address public treasury;
+
+    /// @dev Reverts if contract already initialized
+    error AlreadyInitialized();
+    /// @dev Reverts if receivers and amounts array lengths mismatch
+    error ArraysLengthMismatch();
+    /// @dev Reverts if insufficient token allowance from treasury
+    error NotEnoughApprovedTokens();
+    /// @dev Reverts if ERC20 transfer fails
+    error TransferFailed();
+    /// @dev Reverts if batch size exceeds limit
+    error BatchSizeExceeded();
+
+    /// @dev Restricts `initialize` to one-time execution
+    modifier notInitialized() {
+        require(!initialized, AlreadyInitialized());
+        _;
+    }
+
+    /// @dev Tracks initialization status
+    bool private initialized;
+
+    /// @notice Distributes ERC20 tokens from treasury to recipients
+    /// @param receivers Addresses to receive tokens
+    /// @param amounts Amount of tokens to send per recipient
+    function airdrop(address[] calldata receivers, uint256[] calldata amounts) external onlyOwner {
+        require(receivers.length <= MAX_AIRDROP_BATCH_SIZE, BatchSizeExceeded());
+        require(receivers.length == amounts.length, ArraysLengthMismatch());
+        require(token.allowance(treasury, address(this)) >= amount, NotEnoughApprovedTokens());
+
+        address treasuryAddress = treasury;
+
+        for (uint256 i = 0; i < receivers.length;) {
+            require(token.transferFrom(treasuryAddress, receivers[i], amounts[i]), TransferFailed());
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @inheritdoc IUtilityContract
+    /// @notice Initializes the airdropper contract with required config
+    /// @param _initData Encoded deployManager, token, amount, treasury, and new owner
+    /// @return success True if initialized
+    function initialize(bytes memory _initData) external override notInitialized returns (bool) {
+        (address _deployManager, address _token, uint256 _amount, address _treasury, address _owner) =
+            abi.decode(_initData, (address, address, uint256, address, address));
+
+        setDeployManager(_deployManager);
+
+        token = IERC20(_token);
+        amount = _amount;
+        treasury = _treasury;
+
+        Ownable.transferOwnership(_owner);
+
+        initialized = true;
+        return true;
+    }
+
+    /// @notice Helper to encode constructor-style init data
+    /// @param _deployManager Address of the DeployManager
+    /// @param _token Address of ERC20 token contract
+    /// @param _amount Amount used to validate allowance
+    /// @param _treasury Address holding the tokens
+    /// @param _owner New owner of the contract
+    /// @return Encoded initialization bytes
+    function getInitData(address _deployManager, address _token, uint256 _amount, address _treasury, address _owner)
+        external
+        pure
+        returns (bytes memory)
+    {
+        return abi.encode(_deployManager, _token, _amount, _treasury, _owner);
+    }
+}
